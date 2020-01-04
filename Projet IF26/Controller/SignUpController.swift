8ac138a5f8d693f8061a4ce5ce76d56158982190
Maximilien <regnier.maximilien@gmail.com>
2020-01-04 13:59:25 +0100

    Signup view

---
+++ Projet IF26/Controller/SignUpController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpController.swift
+//  Projet IF26
+//
+//  Created by Maximilien Regnier on 04/01/2020.
+//  Copyright © 2020 Maximilien Regnier. All rights reserved.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
