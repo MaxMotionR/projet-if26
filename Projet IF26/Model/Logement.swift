13b1111d1530b0fe87d41b3f70f345a001df711e
Maximilien <regnier.maximilien@gmail.com>
2020-01-04 21:20:47 +0100

    Logement model

---
+++ Projet IF26/Model/Logement.swift
@@ -0,0 +1,17 @@
+//
+//  Logement.swift
+//  Projet IF26
+//
+//  Created by Maximilien Regnier on 04/01/2020.
+//  Copyright © 2020 Maximilien Regnier. All rights reserved.
+//
+
+import CoreData
+
+class Logement: NSManagedObject{
+        static var all: [Logement] {
+        let request: NSFetchRequest<Logement> = Logement.fetchRequest()
+        guard let logements = try? AppDelegate.viewContext.fetch(request) else { return [] }
+        return logements
+    }
+}
