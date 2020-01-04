02436f684215917144bab0380ae91773f831421b
Maximilien <regnier.maximilien@gmail.com>
2020-01-04 21:29:09 +0100

    Piece Model + get all piece

---
+++ Projet IF26/Model/Piece.swift
@@ -0,0 +1,17 @@
+//
+//  Piece.swift
+//  Projet IF26
+//
+//  Created by Maximilien Regnier on 04/01/2020.
+//  Copyright © 2020 Maximilien Regnier. All rights reserved.
+//
+
+import CoreData
+
+class Piece: NSManagedObject{
+        static var all: [Piece] {
+        let request: NSFetchRequest<Piece> = Piece.fetchRequest()
+        guard let pieces = try? AppDelegate.viewContext.fetch(request) else { return [] }
+        return pieces
+    }
+}
