129dc9d14d7ed55412370db37eab9d658f1f3ef6
Maximilien <regnier.maximilien@gmail.com>
2020-01-04 13:45:26 +0100

    User Class

---
+++ Projet IF26/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Projet IF26
+//
+//  Created by Maximilien Regnier on 04/01/2020.
+//  Copyright © 2020 Maximilien Regnier. All rights reserved.
+//
+
+import CoreData
+
+class User: NSManagedObject{
+    
+}
