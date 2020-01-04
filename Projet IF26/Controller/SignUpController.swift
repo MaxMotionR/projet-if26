e4a49c75eaed0a5505a6ba48051736fe5794329c
Maximilien <regnier.maximilien@gmail.com>
2020-01-04 20:56:06 +0100

    Create new User

--- Projet IF26/Controller/SignUpController.swift
+++ Projet IF26/Controller/SignUpController.swift
@@ -14,4 +14,30 @@ class SignUpController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    @IBAction func singupAction(_ sender: UIButton) {
+        if((loginField.text?.isEmpty ?? false) || (passwordField.text?.isEmpty ?? false) ){
+            let alert = UIAlertController(title: "Erreur", message: "Veuillez compléter tous les champs", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+
+            self.present(alert, animated: true)
+        }else{
+
+            signup(login: loginField.text!, password: passwordField.text!)
+        }
+    }
+    
+    private func signup(login: String, password: String){
+        let user = User(context: AppDelegate.viewContext)
+        user.fullname = login
+        user.password = password
+        
+        try? AppDelegate.viewContext.save()
+        
+        self.dismiss(animated: true)
+    }
 }
