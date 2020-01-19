b55f7b27a41ea6a50ce9dfdf82c5bee68a686360
Maximilien <regnier.maximilien@gmail.com>
2020-01-19 21:15:11 +0100

    Detail Page

--- Projet IF26/Controller/PieceListViewController.swift
+++ Projet IF26/Controller/PieceListViewController.swift
@@ -27,16 +27,14 @@ class PieceListViewController: UIViewController {
         super.viewWillAppear(animated)
         tableView.reloadData()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "segueToDetailList" {
+            let detailListVC = segue.destination as! DetailListViewController
+            detailListVC.piece = self.piece
+        }
     }
-    */
+
 
 }
 
@@ -59,8 +57,8 @@ extension PieceListViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            print("section: \(indexPath.section)")
-            print("row: \(indexPath.row)")
-            print(pieces[indexPath.row])
+        
+        self.piece = self.pieces[indexPath.row]
+        performSegue(withIdentifier: "segueToDetailList", sender: nil)
     }
 }
